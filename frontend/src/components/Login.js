ffef71c4fc047e18823bd5026c6d4f18c16b7394
Adrian Mojica <adrian.mojica@gmail.com>
2021-01-29 17:12:13 -0800

    login token to localstorage

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,40 +1,79 @@
+
+import React, { useState } from "react";
+import axios from 'axios';
 import './Login.scss';
 
 
 function Login() {
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  })
+  
+  function handleChange(evt) {
+    const {name, value} = evt.target;
+    setFormData(data => ({
+      ...data,
+      [name]: value
+    }))
+  }
+
+  function handleSubmit(event){
+    console.log("here");
+    event.preventDefault();
+    setFormData(formData);
+    console.log(formData);
+    loginUser();
+    setFormData({
+      username: "",
+      password: ""
+    })
+  }
+
+  async function loginUser(){
+    let body = { username: formData.username,
+                password: formData.password};
+    let res = await axios.post('http://localhost:3000/login', body);
+    console.log(res);
+    //save to local storage
+    localStorage.setItem('token', res.data.token);
+  }
+
+
   return (
     <>
     <div className="login-register">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-xl-10 col-lg-12 col-md-9">
-            <div class="card o-hidden border-0 shadow-lg my-5">
-              <div class="card-body p-0">
+            <div className="card o-hidden border-0 shadow-lg my-5">
+              <div className="card-body p-0">
                 <div className="row">
                   <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
                   <div className="col-lg-6">
                     <div className="p-5">
-                      <div class="text-center">
-                          <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                      <div className="text-center">
+                          <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
                       </div>
-                      <form class="user">
-                        <div class="form-group">
-                            <input type="email" class="form-control form-control-user"
+                      <form className="user">
+                        <div className="form-group">
+                            <input type="email" className="form-control form-control-user"
                                 id="exampleInputEmail" aria-describedby="emailHelp"
-                                placeholder="Enter Email Address..."/>
+                                placeholder="Enter Email Address..." name="username" id="username" onChange={handleChange} value={formData.username}/>
                         </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control form-control-user"
-                                id="exampleInputPassword" placeholder="Password"/>
+                        <div className="form-group">
+                            <input type="password" className="form-control form-control-user"
+                                id="exampleInputPassword" placeholder="Password" name="password" id="password" onChange={handleChange} value={formData.password}/>
                         </div>
-                        <div class="form-group">
-                            <div class="custom-control custom-checkbox small">
-                                <input type="checkbox" class="custom-control-input" id="customCheck"/>
-                                <label class="custom-control-label" for="customCheck">Remember
+                        <div className="form-group">
+                            <div className="custom-control custom-checkbox small">
+                                <input type="checkbox" className="custom-control-input" id="customCheck"/>
+                                <label className="custom-control-label" htmlFor="customCheck">Remember
                                     Me</label>
                             </div>
                         </div>
-                        <a href="index.html" class="btn btn-primary btn-user btn-block">
+                        <a href="#"  onClick={handleSubmit}  className="btn btn-primary btn-user btn-block">
                             Login
                         </a>
                     </form>
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,48 +1,99 @@
+import React, { useState } from "react";
 import './Register.scss';
-
+import axios from 'axios';
 
 function Register() {
+
+
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    therapist: "DeMarco",
+    password:"",
+    is_admin: false
+  })
+  
+  function handleChange(evt) {
+    const {name, value} = evt.target;
+    setFormData(data => ({
+      ...data,
+      [name]: value
+    }))
+  }
+
+  function handleSubmit(event){
+    console.log("here");
+    event.preventDefault();
+    setFormData(formData);
+    console.log(formData);
+    addUser();
+    setFormData({
+      first_name: "",
+      last_name: "",
+      email: "",
+      therapist: "DeMarco",
+      email:"",
+      password:"",
+      is_admin:false
+    })
+  }
+
+  async function addUser(){ 
+    let body = { username: formData.email,
+                 password: formData.password,
+                 first_name: formData.first_name,
+                 last_name: formData.last_name,
+                 therapist: formData.therapist,
+                 email: formData.email,
+                 is_admin: false
+               };
+    let res = await axios.post('http://localhost:3000/register', body);
+    console.log(res);
+    //TODO: redirect to login
+  }
+  
+
+
+
   return (
     <>
     <div className="login-register">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-xl-10 col-lg-12 col-md-9">
-            <div class="card o-hidden border-0 shadow-lg my-5">
-              <div class="card-body p-0">
+            <div className="card o-hidden border-0 shadow-lg my-5">
+              <div className="card-body p-0">
                 <div className="row">
                   <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
                   <div className="col-lg-6">
                     <div className="p-5">
-                      <div class="text-center">
-                        <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                      <div className="text-center">
+                        <h1 className="h4 text-gray-900 mb-4">Create an Account!</h1>
                       </div>
-                        <form class="user">
-                            <div class="form-group row">
-                                <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                        placeholder="First Name"/>
+                        <form className="user">
+                            <div className="form-group row">
+                                <div className="col-sm-6 mb-3 mb-sm-0">
+                                    <input type="text" className="form-control form-control-user"
+                                        placeholder="First Name" name="first_name" id="first_name" onChange={handleChange} value={formData.first_name}/>
                                 </div>
-                                <div class="col-sm-6">
-                                    <input type="text" class="form-control form-control-user" id="exampleLastName"
-                                        placeholder="Last Name"/>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                    placeholder="Email Address"/>
-                            </div>
-                            <div class="form-group row">
-                                <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input type="password" class="form-control form-control-user"
-                                        id="exampleInputPassword" placeholder="Password"/>
-                                </div>
-                                <div class="col-sm-6">
-                                    <input type="password" class="form-control form-control-user"
-                                        id="exampleRepeatPassword" placeholder="Repeat Password"/>
+                                <div className="col-sm-6">
+                                    <input type="text" className="form-control form-control-user" 
+                                        placeholder="Last Name" name="last_name" id="last_name" onChange={handleChange} value={formData.last_name}/>
                                 </div>
                             </div>
-                            <a href="login.html" class="btn btn-primary btn-user btn-block">
+                            <div className="form-group">
+                                <input type="email" className="form-control form-control-user"
+                                    placeholder="Email Address" name="email" id="email" onChange={handleChange} value={formData.email}/>
+                            </div>
+                            <div className="form-group row">
+                                <div className="col-sm-12 mb-3 mb-sm-0">
+                                    <input type="password" className="form-control form-control-user"
+                                        placeholder="Password" name="password" id="password" onChange={handleChange} value={formData.password}/>
+                                </div>
+                                
+                            </div>
+                            <a href="login.html" onClick={handleSubmit} className="btn btn-primary btn-user btn-block">
                                 Register Account
                             </a>
                         </form>
